2918e9fc87a16164b09f6365abb12495d3955d47
Barry Earsman <barry@earsman.com>
2019-11-19 16:59:07 +1000

    Added messages from cluster admin microservice

--- messaging/responses/subjects/cluster.go
+++ messaging/responses/subjects/cluster.go
@@ -1,5 +1,8 @@
 package subjects
 
 var (
-	CLUSTER_GET_COMPLETED = "bpaas.cluster.get.completed"
+	CLUSTER_CREATE_COMPLETE    = "bpaas.cluster.create.completed"
+	CLUSTER_CREATE_FAILED      = "bpaas.cluster.create.failed"
+	CLUSTER_CREATE_IN_PROGRESS = "bpaas.cluster.create.inprogress"
+	CLUSTER_GET_COMPLETE       = "bpaas.cluster.get.completed"
 )
