7b3942bf397c535a8248326c105e1baf54552a2b
Barry Earsman <barry@earsman.com>
2019-11-19 15:46:11 +1000

    Organise message subjects

--- messaging/requests/subjects/notifications.go
+++ messaging/requests/subjects/notifications.go
@@ -1,6 +1,13 @@
 package subjects
 
 const (
-	EMAIL_INVITE_COMPLETED_SUBJECT = "bpaas.email.invite.completed"
-	EMAIL_INVITE_FAILED_SUBJECT    = "bpaas.email.invite.failed"
+	INVITE_ACCEPTED_SUBJECT                    = "bpaas.notification.invite.accept"
+	INVITE_REJECTED_SUBJECT                    = "bpaas.notification.invite.reject"
+	NOTIFICATION_CHAINCODE_INSTALLED_COMPLETED = "bpaas.notification.chaincode.instantiated"
+	NOTIFICATION_PASSWORD_RESET_SUBJECT        = "bpaas.notification.user.password_reset"
+	NOTIFICATION_PLATFORM_USER_CREATED_SUBJECT = "bpaas.notification.user.platform.created"
+	NOTIFICATION_USER_ACTIVATED_SUBJECT        = "bpaas.notification.user.activated"
+	NOTIFICATION_USER_CREATED_SUBJECT          = "bpaas.notification.user.created"
+	NOTIFICATION_USER_SUSPENDED_SUBJECT        = "bpaas.notification.user.suspended"
+	USER_INVITE_SUBJECT                        = "bpaas.notification.invite"
 )
--- messaging/responses/subjects/notification.go
+++ messaging/responses/subjects/notification.go
@@ -1,13 +1,3 @@
 package subjects
 
-const (
-	INVITE_ACCEPTED_SUBJECT                    = "bpaas.notification.invite.accept"
-	INVITE_REJECTED_SUBJECT                    = "bpaas.notification.invite.reject"
-	NOTIFICATION_CHAINCODE_INSTALLED_COMPLETED = "bpaas.notification.chaincode.instantiated"
-	NOTIFICATION_PASSWORD_RESET_SUBJECT        = "bpaas.notification.user.password_reset"
-	NOTIFICATION_PLATFORM_USER_CREATED_SUBJECT = "bpaas.notification.user.platform.created"
-	NOTIFICATION_USER_ACTIVATED_SUBJECT        = "bpaas.notification.user.activated"
-	NOTIFICATION_USER_CREATED_SUBJECT          = "bpaas.notification.user.created"
-	NOTIFICATION_USER_SUSPENDED_SUBJECT        = "bpaas.notification.user.suspended"
-	USER_INVITE_SUBJECT                        = "bpaas.notification.invite"
-)
+const ()
