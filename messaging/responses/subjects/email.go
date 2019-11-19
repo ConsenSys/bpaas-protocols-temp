b7b102d800c3af53a2aff2e2019258c64357bfcd
Barry Earsman <barry@earsman.com>
2019-11-19 15:21:47 +1000

    Added constants from bpaas-notification

---
+++ messaging/responses/subjects/email.go
@@ -0,0 +1,16 @@
+package subjects
+
+var (
+	EMAIL_ACCEPT_COMPLETED_SUBJECT         = "bpaas.email.accept.completed"
+	EMAIL_ACCEPT_FAILED_SUBJECT            = "bpaas.email.accept.failed"
+	EMAIL_INVITE_COMPLETED_SUBJECT         = "bpaas.email.invite.completed"
+	EMAIL_INVITE_FAILED_SUBJECT            = "bpaas.email.invite.failed"
+	EMAIL_PASSWORD_RESET_COMPLETED_SUBJECT = "bpaas.email.password.reset.completed"
+	EMAIL_PASSWORD_RESET_FAILED_SUBJECT    = "bpaas.email.password.reset.failed"
+	EMAIL_REJECT_COMPLETED_SUBJECT         = "bpaas.email.reject.completed"
+	EMAIL_REJECT_FAILED_SUBJECT            = "bpaas.email.reject.failed"
+	EMAIL_USER_ACTIVATE_COMPLETED_SUBJECT  = "bpaas.email.user.activate.completed"
+	EMAIL_USER_ACTIVATE_FAILED_SUBJECT     = "bpaas.email.user.activate.failed"
+	EMAIL_VERIFY_COMPLETED_SUBJECT         = "bpaas.email.verify.completed"
+	EMAIL_VERIFY_FAILED_SUBJECT            = "bpaas.email.verify.failed"
+)
---
+++ messaging/responses/subjects/notification.go
@@ -0,0 +1,10 @@
+package subjects
+
+const (
+	INVITE_ACCEPTED_SUBJECT                    = "bpaas.notification.invite.accept"
+	INVITE_REJECTED_SUBJECT                    = "bpaas.notification.invite.reject"
+	NOTIFICATION_USER_CREATED_SUBJECT          = "bpaas.notification.user.created"
+	NOTIFICATION_PLATFORM_USER_CREATED_SUBJECT = "bpaas.notification.user.platform.created"
+	NOTIFICATION_PASSWORD_RESET_SUBJECT        = "bpaas.notification.user.password_reset"
+	USER_INVITE_SUBJECT                        = "bpaas.notification.invite"
+)
