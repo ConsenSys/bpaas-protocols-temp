d07375342f5582d6e1eb8160e544dae144348c04
Barry Earsman <barry@earsman.com>
2019-11-19 18:02:56 +1000

    Added cluster request subjects

--- messaging/requests/subjects/cluster.go
+++ messaging/requests/subjects/cluster.go
@@ -1,6 +1,8 @@
 package subjects
 
 var (
-	CLUSTER_GET            = "bpaas.cluster.get"
 	CLUSTER_CREATE_SUBJECT = "bpaas.cluster.create"
+	CLUSTER_DELETE         = "bpaas.cluster.delete"
+	CLUSTER_GET            = "bpaas.cluster.get"
+	CLUSTER_UPDATE         = "bpaas.cluster.update"
 )
