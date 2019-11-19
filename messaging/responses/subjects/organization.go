b81e2c58890178f3050e2b872c6cee2338cdb5f0
Barry Earsman <barry@earsman.com>
2019-11-19 13:32:32 +1000

    Added message request subjects for Organization

---
+++ messaging/responses/subjects/organization.go
@@ -0,0 +1,13 @@
+package subjects
+
+const (
+	ORGANIZATION_CREATE_FAILED_SUBJECT      = "bpaas.organization.create.failed"
+	ORGANIZATION_CREATE_IN_PROGRESS_SUBJECT = "bpaas.organization.create.inprogress"
+	ORGANIZATION_GET_COMPLETED_SUBJECT      = "bpaas.organization.get.completed"
+	ORGANIZATION_LIST_ALL_COMPLETED_SUBJECT = "bpaas.organization.list_all.completed"
+	ORGANIZATION_LIST_ALL_FAILED_SUBJECT    = "bpaas.organization.list_all.failed"
+	ORGANIZATION_LIST_COMPLETED_SUBJECT     = "bpaas.organization.list.completed"
+	ORGANIZATION_LIST_FAILED_SUBJECT        = "bpaas.organization.list.failed"
+	ORGANIZATION_STATUS_COMPLETED_SUBJECT   = "bpaas.organization.status.completed"
+	ORGANIZATION_STATUS_FAILED_SUBJECT      = "bpaas.organization.status.failed"
+)
