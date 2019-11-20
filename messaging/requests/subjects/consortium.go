43cb1292e6a4794697854695b2ff289a198e2a02
Barry Earsman <barry@earsman.com>
2019-11-20 15:04:29 +1000

    Tweaked messages for consistency

--- messaging/requests/subjects/consortium.go
+++ messaging/requests/subjects/consortium.go
@@ -1,7 +1,7 @@
 package subjects
 
 const (
-	CONSORTIUM_CREATE_SUBJECT = "bpaas.consortium.create"
-	CONSORTIUM_GET_SUBJECT    = "bpaas.consortium.get"
-	CONSORTIUM_LIST_SUBJECT   = "bpaas.consortium.list"
+	CONSORTIUM_CREATE = "bpaas.consortium.create"
+	CONSORTIUM_GET    = "bpaas.consortium.get"
+	CONSORTIUM_LIST   = "bpaas.consortium.list"
 )
