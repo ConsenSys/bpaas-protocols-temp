b4dc257842326cd34c8e871265f3eef1b1c6d5d9
Barry Earsman <barry@earsman.com>
2019-11-20 16:14:28 +1000

    Code tidy

--- messaging/requests/subjects/notifications.go
+++ messaging/requests/subjects/notifications.go
@@ -1,15 +1,14 @@
 package subjects
 
 const (
-	NOTIFICATION_CHAINCODE_INSTANTIATED                   = "bpaas.notification.chaincode.instantiated"
-	NOTIFICATION_CONSORTIUM_INVITATION                    = "bpaas.notification.consortium_invitation"
-	NOTIFICATION_EMAIL_LIST_CHAINCODE_INSTANTIATED_FAILED = "bpaas.notification.email_list_chaincode_instantiated.failed"
-	NOTIFICATION_INVITE                                   = "bpaas.notification.invite"
-	NOTIFICATION_INVITE_ACCEPT                            = "bpaas.notification.invite.accept"
-	NOTIFICATION_INVITE_REJECT                            = "bpaas.notification.invite.reject"
-	NOTIFICATION_PASSWORD_RESET                           = "bpaas.notification.user.password_reset"
-	NOTIFICATION_PLATFORM_USER_CREATED                    = "bpaas.notification.user.platform.created"
-	NOTIFICATION_USER_ACTIVATED                           = "bpaas.notification.user.activated"
-	NOTIFICATION_USER_CREATED                             = "bpaas.notification.user.created"
-	NOTIFICATION_USER_SUSPENDED                           = "bpaas.notification.user.suspended"
+	NOTIFICATION_CHAINCODE_INSTANTIATED = "bpaas.notification.chaincode.instantiated"
+	NOTIFICATION_CONSORTIUM_INVITATION  = "bpaas.notification.consortium_invitation"
+	NOTIFICATION_INVITE                 = "bpaas.notification.invite"
+	NOTIFICATION_INVITE_ACCEPT          = "bpaas.notification.invite.accept"
+	NOTIFICATION_INVITE_REJECT          = "bpaas.notification.invite.reject"
+	NOTIFICATION_PASSWORD_RESET         = "bpaas.notification.user.password_reset"
+	NOTIFICATION_PLATFORM_USER_CREATED  = "bpaas.notification.user.platform.created"
+	NOTIFICATION_USER_ACTIVATED         = "bpaas.notification.user.activated"
+	NOTIFICATION_USER_CREATED           = "bpaas.notification.user.created"
+	NOTIFICATION_USER_SUSPENDED         = "bpaas.notification.user.suspended"
 )
