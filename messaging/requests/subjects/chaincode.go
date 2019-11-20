19b9b7cd30700c267741002ad32bb2baebf2fc83
Barry Earsman <barry@earsman.com>
2019-11-20 12:33:20 +1000

    Added chaincode instantiate message

--- messaging/requests/subjects/chaincode.go
+++ messaging/requests/subjects/chaincode.go
@@ -1,6 +1,7 @@
 package subjects
 
 const (
-	CHAINCODE_LIST_SUBJECT    = "bpaas.chaincode.list"
-	CHAINCODE_INSTALL_SUBJECT = "bpaas.chaincode.install"
+	CHAINCODE_LIST_SUBJECT        = "bpaas.chaincode.list"
+	CHAINCODE_INSTALL_SUBJECT     = "bpaas.chaincode.install"
+	CHAINCODE_INSTANTIATE_SUBJECT = "bpaas.chaincode.instantiate"
 )
