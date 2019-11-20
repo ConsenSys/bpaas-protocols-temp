2c456e7ee8aef8a05fe19f33edd3539443147240
Barry Earsman <barry@earsman.com>
2019-11-20 12:37:05 +1000

    Corrected subject names

--- messaging/responses/subjects/cluster.go
+++ messaging/responses/subjects/cluster.go
@@ -1,13 +1,13 @@
 package subjects
 
 var (
-	CLUSTER_CREATE_COMPLETE    = "bpaas.cluster.create.completed"
+	CLUSTER_CREATE_COMPLETED   = "bpaas.cluster.create.completed"
 	CLUSTER_CREATE_FAILED      = "bpaas.cluster.create.failed"
 	CLUSTER_CREATE_IN_PROGRESS = "bpaas.cluster.create.inprogress"
 	CLUSTER_DELETE_COMPLETE    = "bpaas.cluster.delete.completed"
 	CLUSTER_DELETE_FAILED      = "bpaas.cluster.delete.failed"
-	CLUSTER_GET_COMPLETE       = "bpaas.cluster.get.completed"
+	CLUSTER_GET_COMPLETED      = "bpaas.cluster.get.completed"
 	CLUSTER_GET_FAILED         = "bpaas.cluster.get.failed"
-	CLUSTER_UPDATE_COMPLETE    = "bpaas.cluster.update.completed"
+	CLUSTER_UPDATE_COMPLETED   = "bpaas.cluster.update.completed"
 	CLUSTER_UPDATE_FAILED      = "bpaas.cluster.update.failed"
 )
