0bee8c7210ed0e85102aa9384b847bc097bd7aad
Barry Earsman <barry@earsman.com>
2019-11-20 15:22:26 +1000

    Tweaked messages for consistency

--- messaging/responses/subjects/token.go
+++ messaging/responses/subjects/token.go
@@ -1,10 +1,10 @@
 package subjects
 
 const (
-	TOKEN_CREATE_COMPLETE = "bpaas.token.create.completed"
-	TOKEN_CREATE_FAILED   = "bpaas.token.create.failed"
-	TOKEN_EXPIRE_COMPLETE = "bpaas.token.expire.completed"
-	TOKEN_EXPIR_FAILED    = "bpaas.token.expire.failed"
-	TOKEN_GET_COMPLETE    = "bpaas.token.get.completed"
-	TOKEN_GET_FAILED      = "bpaas.token.get.failed"
+	TOKEN_CREATE_COMPLETED = "bpaas.token.create.completed"
+	TOKEN_CREATE_FAILED    = "bpaas.token.create.failed"
+	TOKEN_EXPIRE_COMPLETED = "bpaas.token.expire.completed"
+	TOKEN_EXPIRE_FAILED    = "bpaas.token.expire.failed"
+	TOKEN_GET_COMPLETED    = "bpaas.token.get.completed"
+	TOKEN_GET_FAILED       = "bpaas.token.get.failed"
 )
