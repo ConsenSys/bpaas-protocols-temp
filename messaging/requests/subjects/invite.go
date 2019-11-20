3b84096a482a120921ee79c87c0bcb0724d4a624
Barry Earsman <barry@earsman.com>
2019-11-20 15:15:44 +1000

    Tweaked messages for consistency

--- messaging/requests/subjects/invite.go
+++ messaging/requests/subjects/invite.go
@@ -1,8 +1,8 @@
 package subjects
 
 const (
-	INVITE_ACCEPT_SUBJECT = "bpaas.invite.accept"
-	INVITE_LIST_SUBJECT   = "bpaas.invite.list"
-	INVITE_REJECT_SUBJECT = "bpaas.invite.reject"
-	INVITE_SEND_SUBJECT   = "bpaas.invite.send"
+	INVITE_ACCEPT = "bpaas.invite.accept"
+	INVITE_LIST   = "bpaas.invite.list"
+	INVITE_REJECT = "bpaas.invite.reject"
+	INVITE_SEND   = "bpaas.invite.send"
 )
--- messaging/requests/subjects/role.go
+++ messaging/requests/subjects/role.go
@@ -1,9 +1,9 @@
 package subjects
 
 const (
-	LIST_ASSIGNABLE_COMPANY_ROLE_SUBJECT  = "bpaas.roles.list_assignable_company_roles"
-	LIST_ASSIGNABLE_PLATFORM_ROLE_SUBJECT = "bpaas.roles.list_assignable_platform_roles"
-	LIST_COMPANY_ROLE_SUBJECT             = "bpaas.roles.list_company_roles"
-	LIST_PLATFORM_ROLE_SUBJECT            = "bpaas.roles.list_platform_roles"
-	UPDATE_COMPANY_USER_SUBJECT           = "bpaas.company.update_user_role"
+	LIST_ASSIGNABLE_COMPANY_ROLE  = "bpaas.roles.list_assignable_company_roles"
+	LIST_ASSIGNABLE_PLATFORM_ROLE = "bpaas.roles.list_assignable_platform_roles"
+	LIST_COMPANY_ROLE             = "bpaas.roles.list_company_roles"
+	LIST_PLATFORM_ROLE            = "bpaas.roles.list_platform_roles"
+	UPDATE_COMPANY_USER           = "bpaas.company.update_user_role"
 )
