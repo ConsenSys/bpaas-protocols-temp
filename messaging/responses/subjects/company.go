644d2f1dce6540065e96eb5a96361be60d49bed1
Barry Earsman <barry@earsman.com>
2019-11-22 13:30:57 +1000

    Fixed incorrect message subject const

--- messaging/responses/subjects/company.go
+++ messaging/responses/subjects/company.go
@@ -11,7 +11,7 @@ const (
 	COMPANY_USER_REMOVE_FAILED          = "bpaas.company.user.remove.failed"
 	COMPANY_USER_ACTIONS_LIST_COMPLETED = "bpaas.company.user.actions.list.completed"
 	COMPANY_USER_ACTIONS_LIST_FAILED    = "bpaas.company.user.actions.list.failed"
-	COMPANY_USER_ROLE_UPDATE            = "bpaas.company.user.role.update.completed"
+	COMPANY_USER_ROLE_UPDATE_COMPLETED  = "bpaas.company.user.role.update.completed"
 	COMPANY_USER_ROLE_UPDATE_FAILED     = "bpaas.company.user.role.update.failed"
 	COMPANY_USER_UPDATE_COMPLETED       = "bpaas.company.user.update.completed"
 	COMPANY_USER_UPDATE_FAILED          = "bpaas.company.user.update.failed"
