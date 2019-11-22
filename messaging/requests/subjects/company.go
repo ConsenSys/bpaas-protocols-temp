84f7b3ae3e5c35b5c2f7ecf6e763502b6d8dd9a1
Barry Earsman <barry@earsman.com>
2019-11-22 13:23:07 +1000

    Made Company messages more consistent

--- messaging/requests/subjects/company.go
+++ messaging/requests/subjects/company.go
@@ -1,10 +1,11 @@
 package subjects
 
 const (
-	COMPANY_CREATE                = "bpaas.company.create"
-	COMPANY_GET_USER_ACTIONS      = "bpaas.company.get_user_actions"
-	COMPANY_LIST_ALL              = "bpaas.company.list_all"
-	COMPANY_LIST                  = "bpaas.company.list"
-	COMPANY_REMOVE_USER           = "bpaas.company.remove_user"
-	COMPANY_USER_UPDATE_USER_ROLE = "bpaas.company.update_user_role"
+	COMPANY_CREATE            = "bpaas.company.create"
+	COMPANY_LIST_ALL          = "bpaas.company.list_all"
+	COMPANY_LIST              = "bpaas.company.list"
+	COMPANY_USER_ACTIONS_LIST = "bpaas.company.user.actions.list"
+	COMPANY_USER_REMOVE       = "bpaas.company.user.remove"
+	COMPANY_USER_ROLE_UPDATE  = "bpaas.company.user.role.update"
+	COMPANY_USER_UPDATE       = "bpaas.company.user.update"
 )
