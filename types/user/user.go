fbc3c8b27ad7ff1b2ef5e59b3a220bb5fe15a778
Yusuf Kelo <yusufkel@gmail.com>
2019-12-15 16:18:58 +0400

    added createuserrequest type

---
+++ types/user/user.go
@@ -0,0 +1,8 @@
+package user
+
+//CreateUserRequest parameters
+type CreateUserRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Token    string `json:"token"`
+}
